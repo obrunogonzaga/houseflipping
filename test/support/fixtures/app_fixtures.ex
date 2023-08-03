4a59d9a6c68e12267a55ce6b742d94f6c558c10f
Bruno Gonzaga Santos <brunog86@gmail.com>
2023-08-03 21:19:15 +0000

    Adding user structured

---
+++ test/support/fixtures/app_fixtures.ex
@@ -0,0 +1,22 @@
+defmodule Houseflipping.AppFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Houseflipping.App` context.
+  """
+
+  @doc """
+  Generate a user.
+  """
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{
+        email: "some email",
+        name: "some name",
+        password: "some password"
+      })
+      |> Houseflipping.App.create_user()
+
+    user
+  end
+end
